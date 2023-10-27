7f09d8ea5287e59d6d053e955a5697302afdf0c9
umairgul <umairguluol@gmail.com>
2023-10-27 18:35:12 +0500

    admin layout, sidebar and dashboard file

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,31 @@
+<aside class="navbar navbar-vertical navbar-expand-sm navbar-dark">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <h1 class="navbar-brand navbar-brand-autodark">
+            <a href="#">
+                <img src="https://preview.tabler.io/static/logo-white.svg" width="110" height="32" alt="Tabler"
+                    class="navbar-brand-image">
+            </a>
+        </h1>
+        <div class="collapse navbar-collapse" id="sidebar-menu">
+            <ul class="navbar-nav pt-lg-3">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('admin.dashboard') }}">
+                        <span class="nav-link-title">
+                            Dashboard
+                        </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <span class="nav-link-title">
+                            Profile
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</aside>
