9650f8e4e30a034b583eb5284e7710589fe62160
umairgul <umairguluol@gmail.com>
2023-10-31 16:45:44 +0500

    added tabler icons cdn

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -5,41 +5,42 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
+    {{-- CSRF Token --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
+    {{-- Fonts --}}
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/js/tabler.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/core@1.0.0-beta17/dist/css/tabler.min.css">
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@tabler/icons-webfont@2.36.0/tabler-icons.min.css">
 </head>
 
 <body>
     <div class="page">
-        <!-- Sidebar -->
+        {{-- Sidebar --}}
         @include('admin.sidebar')
         <div class="page-wrapper">
-          <div class="page-header d-print-none">
-            <div class="container-xl">
-              <div class="row g-2 align-items-center">
-                <div class="col">
-                  <h2 class="page-title">
-                    {{ $heading ?? 'Dashboard' }}
-                  </h2>
+            <div class="page-header d-print-none">
+                <div class="container-xl">
+                    <div class="row g-2 align-items-center">
+                        <div class="col">
+                            <h2 class="page-title">
+                                {{ $pageTitle ?? 'Dashboard' }}
+                            </h2>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-          <div class="page-body">
-            @yield('content')
-          </div>
+            {{-- Body / Content --}}
+            <div class="page-body">
+                @yield('content')
+            </div>
         </div>
-      </div>
+    </div>
 </body>
 
 </html>
